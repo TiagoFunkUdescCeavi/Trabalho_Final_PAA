8f39bf3803474b8bea6c68d04a3f402083cded73
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-06-20 23:19:23 -0300

    add timestamp to execution

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+// #include <omp.h>
+#include <time.h>
 #include "stdio.h"
 #include "stdlib.h"
 
@@ -5,7 +7,7 @@
 
 #include "Escalonador_Tarefas_Publico.h"
 
-int main() {
+void run_resolver(int print) {
   pAuditorio pAuditorio = criar_auditorio(SIZE);
   upsert_atividade(pAuditorio, 0, 2, 8);
   upsert_atividade(pAuditorio, 1, 1, 5);
@@ -20,11 +22,43 @@ int main() {
   upsert_atividade(pAuditorio, 10, 2, 13);
   upsert_atividade(pAuditorio, 11, 3, 8);
 
-  print_auditorio(pAuditorio);
+  if (print > 0)
+    print_auditorio(pAuditorio);
   resolver(pAuditorio);
-  print_auditorio(pAuditorio);
+  if (print > 0)
+    print_auditorio(pAuditorio);
 
   destruir_auditorio(pAuditorio);
+}
+
+void run_auditorio(int execution, int times) {
+  clock_t begin = clock();
+
+  for (int i = 0; i < times; i++) {
+    run_resolver(0);
+  }
+
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("%d;%d;%f", execution, times, end - time_spent);
+}
+
+int main(int argc, char** agrv) {
+  if (argc <= 1) {
+    run_resolver(1);
+    return 0;
+  }
+
+  if (argc != 3) {
+    printf("Wrong number of params, it was expected to be 2 parameters or none");
+    return 1;
+  }
+
+  int execution = atoi(agrv[1]);
+  int limit = atoi(agrv[2]);
+
+  run_auditorio(execution, limit);
 
   return 0;
 }
--- script/generate_csv.sh
+++ script/generate_csv.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 CSV_FILE=./assets/data.csv
-BINARY=main
-EXECUTIONS=20
+BINARY=TRABALHO_FINAL_PAA
+EXECUTIONS=1000
 
 generate_csv() {
 
-  numbers=(10 100 1000 10000 100000)
+  numbers=(10 100 1000 10000 100000 1000000 10000000)
 
-  echo "EXECUTION;NUMBER" >> $CSV_FILE
+  echo "EXECUTION;NUMBER;TIME" >> $CSV_FILE
   for number in "${numbers[@]}"
   do
     for exec in `seq $EXECUTIONS`
     do
       echo "Excution ${exec} for number ${number}"
-        # ./$BINARY $exec $number >> $CSV_FILE
-        # echo >> $CSV_FILE
+      ./$BINARY $exec $number >> $CSV_FILE
+      echo >> $CSV_FILE
     done
   done
 
@@ -23,5 +23,5 @@ generate_csv() {
 
 rm $CSV_FILE
 make clean
-  # make
+make
 generate_csv
